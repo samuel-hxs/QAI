338e9419979bac230d42c07aa228d970f9dfd2b1
Hex3Dnone <samuel.traue@gmail.com>
2018-04-08 17:42:00 +0200

    Test of irc3 as plugin inside qai

--- qai/__main__.py
+++ qai/__main__.py
@@ -3,6 +3,23 @@ import irc3
 from qai.irc.plugin import Plugin
 
 if __name__ == '__main__':
-    irc = Plugin(irc3.IrcBot())
+    config = dict(
+        nick='QAI', autojoins=['#QAI'],
+        host='localhost', port=6667, ssl=False,
+        includes=[
+            'irc3.plugins.core',
+            'irc3.plugins.command',
+            'irc3.plugins.human',
+            __name__,  # this register the plugin
+        ]
+    )
+    bot = irc3.IrcBot.from_config(config)
+    irc = Plugin(bot)
 
+    try:
+        bot.run(forever=True)
+    except ConnectionRefusedError as ex:
+        print(ex)
+    except LookupError as ex:
+        print(ex)
     exit(0)
--- qai/irc/plugin.py
+++ qai/irc/plugin.py
@@ -1,13 +1,25 @@
 # -*- coding: utf-8 -*-
+from irc3 import rfc
 from irc3.plugins.command import command
 import irc3
 
 
 @irc3.plugin
 class Plugin:
+    """A plugin is a class which take the IrcBot as argument
+        """
+
+    requires = [
+        'irc3.plugins.core',
+        'irc3.plugins.userlist',
+        'irc3.plugins.command',
+        'irc3.plugins.human',
+    ]
+
     def __init__(self, bot):
         self.bot = bot
-        self.bot.run()
+        # self.bot.include('irc3.plugins.logger')
+        # self.bot.include('irc3.plugins.command')
 
     @irc3.event(irc3.rfc.JOIN)
     def say_hi(self, mask, channel, **kw):
@@ -17,10 +29,14 @@ class Plugin:
         else:
             self.bot.privmsg(channel, 'Hi!')
 
-    @command(permission='view')
+    @command(permission='view', public=True)
     def echo(self, mask, target, args):
         """Echo
 
             %%echo <message>...
         """
         yield ' '.join(args['<message>'])
+
+    @irc3.event(rfc.ERR_NOSUCHSERVER)
+    def myevent(bot, srv=None, me=None, server=None, data=None):
+        pass
